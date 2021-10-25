cc6344c48ba2e884550f0a8bafe87a5ed3ae135a
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2021-10-25 19:17:36 +0200

    configure webpack to use @apply directive inside vue components

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,9 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.options({
+    postCss: [require("tailwindcss")("./tailwind.config.js")]
+})
 
 mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
