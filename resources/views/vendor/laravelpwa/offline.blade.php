0f61b0604b6c158b538a08b6ced3bef02b994460
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2021-11-07 22:18:36 +0100

    param pwa

---
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -0,0 +1,7 @@
+{{--@extends('layouts.app')--}}
+@extends('errors.illustrated-layout')
+
+@section('title', __('You are currently not connected to any networks.'))
+{{--@section('code', '404')--}}
+@section('message', __('You are currently not connected to any networks.'))
+
