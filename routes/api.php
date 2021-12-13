f59740b7baee1fad894de487e42d0dc78c6d73c4
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2021-12-13 22:28:59 +0100

    add GET /api/spots

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/spots', function () {
+    return response(\App\Models\Spot::all());
+});
