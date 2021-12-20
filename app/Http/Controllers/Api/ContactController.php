d92c1e11e601edaf54e230a1d71e05dcb9d690c3
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2021-12-20 16:53:30 +0100

    add POST /api/contact route

---
+++ app/Http/Controllers/Api/ContactController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Mail\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function send(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'nullable|string|max:200',
+            'email' => 'email|required',
+            'subject' => 'nullable|string|max:255',
+            'content' => 'string',
+        ]);
+
+        Mail::to(config('mail.contact.address'))->send(new Contact($validated));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/contact', [\App\Http\Controllers\Api\ContactController::class, 'send']);
+
 Route::get('/spots', [\App\Http\Controllers\Api\SpotsController::class, 'index']);
 Route::get('/spots/{spot}/forecast', [\App\Http\Controllers\Api\SpotsController::class, 'getForecast']);
