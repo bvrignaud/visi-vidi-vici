34ece72ce25d70652e8765f54015b3b36c4bb75a
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2023-07-11 23:46:41 +0200

    refactor: fix phpstan errors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -11,7 +11,7 @@ class Kernel extends HttpKernel
      *
      * These middleware are run during every request to your application.
      *
-     * @var array<string>
+     * @var array<int, class-string|string>
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
@@ -27,7 +27,7 @@ class Kernel extends HttpKernel
     /**
      * The application's route middleware groups.
      *
-     * @var array<string, array<string>>
+     * @var array<string, array<int, class-string|string>>
      */
     protected $middlewareGroups = [
         'web' => [
--- app/Http/Resources/WebcamResource.php
+++ app/Http/Resources/WebcamResource.php
@@ -10,7 +10,8 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class WebcamResource extends JsonResource
 {
     /**
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
+     * @return array<string, mixed>
      */
     public function toArray($request): array
     {
