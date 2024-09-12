4ed4c73b901d7f7d3aac2102014b89177a8c9632
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2024-09-12 08:30:13 +0200

    chore: remove barryvdh/laravel-ide-helper

--- app/Models/Spot.php
+++ app/Models/Spot.php
@@ -6,33 +6,6 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * App\Models\Spot
- *
- * @property string $id
- * @property string $name
- * @property float $lng
- * @property float $lat
- * @property int $optimal_wind_direction
- * @property string $timezone
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- *
- * @method static \Database\Factories\SpotFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Spot newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Spot newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Spot query()
- * @method static \Illuminate\Database\Eloquent\Builder|Spot whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Spot whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Spot whereLat($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Spot whereLng($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Spot whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Spot whereOptimalWindDirection($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Spot whereTimezone($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Spot whereUpdatedAt($value)
- *
- * @mixin \Eloquent
- */
 class Spot extends Model
 {
     use HasFactory, HasUuids;
