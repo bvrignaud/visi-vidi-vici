cd96f35806f0e7f50fc0344d0564974a1d8099db
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2024-10-03 18:08:27 +0200

    feat: Types/Forecast.ts

---
+++ resources/js/Types/Forecast.ts
@@ -0,0 +1,12 @@
+
+interface Forecast {
+  note: number,
+  airTemperature: number,
+  cloudCover: number,
+  swellHeight: number,
+  waterTemperature: number,
+  windDirection: number,
+  windSpeed: number,
+}
+
+export default Forecast;
