cd45e1fd75dd1ca2792a29d137139ec3af3ca444
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2024-10-04 10:50:23 +0200

    chore: upgrage missing parts

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,24 +5,26 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { i18nVue } from 'laravel-vue-i18n';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Visi vidi vici';
 
 createInertiaApp({
-    title: (title) => `${title} 🐟 ${appName}`,
-  progress: { color: '#4B5563' },
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+  title: (title) => `${title} 🐟 ${appName}`,
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(i18nVue, {
-              resolve: async lang => {
-                const langs = import.meta.glob('../../lang/*.json');
-                return await langs[`../../lang/${lang}.json`]();
-              }
-            })
-            .use(ZiggyVue, Ziggy)
-            .mount(el);
-    },
+      .use(plugin)
+      .use(i18nVue, {
+        resolve: async lang => {
+          const langs = import.meta.glob('../../lang/*.json');
+          return await langs[`../../lang/${lang}.json`]();
+        }
+      })
+      .use(ZiggyVue)
+      .mount(el);
+  },
+  progress: {
+    color: '#4B5563',
+  },
 });
