31087a26c2c3a3d99289c65120eb6c65bc2ab104
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2025-05-13 20:07:18 +0200

    refactor: rename paths to use consistent casing for "Profile"
    Updated all "profile" paths to "Profile" for uniform casing across the codebase. This improves consistency and aligns with naming conventions.

--- resources/js/pages/Profile/Show.vue
+++ resources/js/pages/Profile/Show.vue
@@ -1,7 +1,7 @@
 <template>
   <app-layout title="Profil">
     <template #header>
-      <h2 class="text-xl font-semibold leading-tight text-gray-800">Profil</h2>
+      <h2 class="text-xl leading-tight font-semibold text-gray-800">Profil</h2>
     </template>
 
     <div>
@@ -39,11 +39,11 @@
 <script lang="ts">
 import JetSectionBorder from '@/jetstream/SectionBorder.vue'
 import AppLayout from '@/layouts/AppLayout.vue'
-import DeleteUserForm from '@/pages/profile/Partials/DeleteUserForm.vue'
-import LogoutOtherBrowserSessionsForm from '@/pages/profile/Partials/LogoutOtherBrowserSessionsForm.vue'
-import TwoFactorAuthenticationForm from '@/pages/profile/Partials/TwoFactorAuthenticationForm.vue'
-import UpdatePasswordForm from '@/pages/profile/Partials/UpdatePasswordForm.vue'
-import UpdateProfileInformationForm from '@/pages/profile/Partials/UpdateProfileInformationForm.vue'
+import DeleteUserForm from '@/pages/Profile/Partials/DeleteUserForm.vue'
+import LogoutOtherBrowserSessionsForm from '@/pages/Profile/Partials/LogoutOtherBrowserSessionsForm.vue'
+import TwoFactorAuthenticationForm from '@/pages/Profile/Partials/TwoFactorAuthenticationForm.vue'
+import UpdatePasswordForm from '@/pages/Profile/Partials/UpdatePasswordForm.vue'
+import UpdateProfileInformationForm from '@/pages/Profile/Partials/UpdateProfileInformationForm.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
