eb9ec421f3129873d82aa9b708f90e480127a15d
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2025-07-08 15:32:37 +0200

    feat(buttons): add BuyMeACoffeeButton component
    Introduced a reusable `BuyMeACoffeeButton` component for improved modularity and reusability. Replaced the hardcoded button in `AppLayout` with the new component and updated the button index export to include it. This change enhances consistency and simplifies future maintenance.

---
+++ resources/js/components/ui/buttons/BuyMeACoffeeButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <a href="https://www.buymeacoffee.com/bvrignaud" target="_blank">
+    <img
+      class="h-13"
+      src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=bvrignaud&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff"
+      alt="Buy me a coffee"
+    />
+  </a>
+</template>
--- resources/js/components/ui/buttons/index.ts
+++ resources/js/components/ui/buttons/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button.vue'
+export { default as BuyMeACoffeeButton } from './BuyMeACoffeeButton.vue'
 export { default as DangerButton } from './DangerButton.vue'
 export { default as SecondaryButton } from './SecondaryButton.vue'
