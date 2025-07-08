931e0e12409ffb895eb952cb52191a162c7a2fc7
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2025-07-08 15:02:46 +0200

    fix(modal): add close button with accessibility support
    Introduced a close button with proper styling and accessibility features to the modal component. Leveraged hover and focus states for better user experience and included an assistive text for screen readers. This enhances both usability and accessibility of the modal.

--- resources/js/components/ui/modal/Modal.vue
+++ resources/js/components/ui/modal/Modal.vue
@@ -29,6 +29,29 @@
             :class="maxWidthClass"
           >
             <slot v-if="show"></slot>
+            <button
+              v-if="closeable"
+              type="button"
+              class="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-none disabled:pointer-events-none"
+              @click="close"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-xicon h-4 w-4"
+              >
+                <path d="M18 6 6 18"></path>
+                <path d="m6 6 12 12"></path>
+              </svg>
+              <span class="sr-only">Close</span>
+            </button>
           </div>
         </transition>
       </div>
