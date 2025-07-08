f49b93eee99eb56b0ea676b869d0e3f6f5124681
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2025-07-08 15:42:05 +0200

    feat(storybook): add story for BuyMeACoffeeButton component
    Created a Storybook story for the `BuyMeACoffeeButton` component to aid in visual testing and documentation.

---
+++ resources/js/components/ui/buttons/BuyMeACoffeeButton.story.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { BuyMeACoffeeButton } from '@/components/ui/buttons'
+</script>
+
+<template>
+  <Story title="Ui/Buttons/BuyMeACoffeeButton">
+    <buy-me-a-coffee-button />
+  </Story>
+</template>
